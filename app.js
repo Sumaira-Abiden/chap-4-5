d074e94980d871d2f693b94238a6bd1ce68afd5b
Sumaira-Abiden <sumairaabiden@gmail.com>
2024-10-11 23:35:25 +0500

    chap 4 to 5

---
+++ app.js
@@ -0,0 +1,241 @@
+// Question 1
+// let a = 1, b = 2, c = 3;
+
+// Question 2
+// legal varaible
+// myVariable
+// age123
+// first_name
+// isValid
+// totalAmount
+
+// illegal Variable
+// 1stVariable (cannot start with a digit)
+// total amount (cannot contain spaces)
+// my-variable (often not allowed due to special characters)
+// class (reserved keyword in many languages)
+// var!name (contains an illegal special character)
+
+// Question 3
+// document.write("<h1>"+ "Rules for naming JS Variables" + "</h1>")
+// document.write ("variables name conatin number $ and_")
+// document.write("</br>")
+// document.write("variable must begin with letter $ or _")
+// document.write("</br>")
+// document.write("Variables names are case sensitive")
+// document.write("variable name should no be JS keyword")
+
+// Chapter 5
+// Question 1
+// let a = 3
+// let b = 5
+// let c= a+b
+// console.log(c);
+// document.write(`sum of ${a} and ${b} is ${c}`)
+
+// Question 2
+// let a = 5
+// let b = 3
+// let c= a-b
+// console.log(c);
+// document.write(`subtract of ${a} and ${b} is ${c}`)
+
+// Question 2
+// let a = 5
+// let b = 3
+// let c= a*b
+// console.log(c);
+// document.write(`multiply of ${a} and ${b} is ${c}`)
+
+// Question 2
+//   let a = 5
+// let b = 3
+// let c= a/b
+// console.log(c);
+// document.write(`divide of ${a} and ${b} is ${c}`)
+
+// Question 2
+// let a = 5
+// let b = 3
+// let c= a%b
+// console.log(c);
+// document.write(`modulus of ${a} and ${b} is ${c}`)
+// document.write('sum of ' + sum1 +' and '+ sum2 + ' is '+ total)
+
+// Question 3
+// let a
+// document.write(`Value after variable decleration is ${a} <br>`);
+
+// a = 5
+// document.write(`Initial Variable ${a} <br>`);
+
+// let increment = ++a
+// document.write(`value after increment is :  ${increment} <br> `);
+
+// let b = 7
+
+// let add = a + b
+// document.write(`value after adding is :  ${add} <br>`);
+
+// let decrement = --add
+// document.write(`value after decrement is :  ${add} <br>`);
+
+// let Reminder = decrement%3
+// document.write(`The remainder is :  ${Reminder} <br>`);
+
+// Question 4
+
+// let movieticket
+// movieticket = 600
+// let ticketprice
+// ticketprice = 5
+// let totalcost = movieticket * ticketprice
+// document.write(`Total cost to buy   ${ticketprice} ticket to a movie is   ${totalcost} `);
+
+// Question 5
+
+// document.write(' Table of 4 ')
+// document.write('<br>')
+// document.write(4 , ' x 1 = ', 4*1 )
+// document.write('<br>')
+// document.write(4 , ' x 2 = ', 4*2 )
+// document.write('<br>')
+// document.write(4 , ' x 3 = ', 4*3 )
+// document.write('<br>')
+// document.write(4 , ' x 4 = ', 4*4 )
+// document.write('<br>')
+// document.write(4 , ' x 5 = ', 4*5 )
+// document.write('<br>')
+// document.write(4 , ' x 6 = ', 4*6 )
+// document.write('<br>')
+// document.write(4 , ' x 7 = ', 4*7 )
+// document.write('<br>')
+// document.write(4 , ' x 8 = ', 4*8 )
+// document.write('<br>')
+// document.write(4 , ' x 9 = ', 4*9 )
+// document.write('<br>')
+// document.write(4 , ' x 10 = ', 4*10 )
+
+// Question 6
+
+// document.write("<h2>Temperature Converter</h2> ");
+
+// let temperatureInDegree = 25;
+// // F= (C * 9/5) + 32
+
+// let temperatureInFahrenheight = (temperatureInDegree * 9) / 5 + 32;
+// document.write(temperatureInDegree, " C is ", temperatureInFahrenheight, " F ");
+
+// document.write("<br>");
+
+// let fahrenheittem = 70;
+// // C= (F * 9/5) + 32
+
+// let celsius = ((fahrenheittem - 32) * 5) / 9;
+// document.write(fahrenheittem, " F is ", celsius, " C ");
+// document.write("<hr>");
+
+// Question 7
+
+// let price_item1
+// price_item1 = 650
+// document.write( `price of item 1 is ${price_item1} </br>` )
+
+// let order_Quantity1
+// order_Quantity1 = 3
+// document.write( `Quantity of item 1 is ${order_Quantity1} </br>` )
+
+// let price_item2
+// price_item2 = 100
+// document.write( `price of item 2 is ${price_item2} </br>` )
+
+// let order_Quantity2
+// order_Quantity2 = 7
+// document.write( `Quantity of item 2 is ${order_Quantity2} </br>` )
+
+// let shipping_Charges
+// shipping_Charges = 100
+// document.write( `Shipping charges is ${shipping_Charges} </br>` )
+
+// let total_cost = price_item1 * order_Quantity1 + price_item2 * order_Quantity2  + 100
+// document.write(`Total cost of your order is  ${total_cost}  `);
+
+// Question 8
+
+//  let total_marks
+//  total_marks = 980
+//  document.write(`total marks ${total_marks} </br>`)
+
+//   let marks_obtained
+//  marks_obtained = 804
+//  document.write(` marks obtaied ${marks_obtained} </br>`)
+
+//   let percentage = (marks_obtained / total_marks) * 100;
+
+//   document.write(` Percentage ${percentage} `)
+
+// Question 9
+
+// let US_dollars
+// US_dollars = 10
+// let Suadi_Riyals
+// Suadi_Riyals = 25
+// let Exchange_rates = US_dollars* 104.80 + Suadi_Riyals * 28
+
+// document.write(`Total currency in PKR ${Exchange_rates}`)
+
+// Question 10
+// let add = 5
+// let multiply = 10
+// let divide = 2
+// let result = (add*multiply/2)
+// document.write(result)
+
+// Question 11
+// let current_year
+// current_year = 2016
+// document.write(` Current year ${current_year} </br>`)
+
+// let birth_year
+// birth_year = 1992
+// document.write(` Birth year ${birth_year} </br>`)
+
+// let age
+// age = 1992
+// document.write(` Your age is ${age} </br>`)
+
+// Question 12
+
+// let radiuscircle = 20;
+// document.write("Radius of a circle: ", radiuscircle);
+// document.write("<br>");
+
+// let circumference = 2 * 3.142 * 20;
+// document.write("The Circumference: " + circumference);
+// document.write("<br>");
+
+// let area = 3.142 * 20 * 20;
+// document.write("The area is: " + area);
+
+
+// Q13
+
+// document.write('<h1>The Lifetime Supply Calculator</h2>')
+// let snack = " Chocolate Chip"
+// document.write('Favorite snack:', snack)
+// document.write('<br>')
+
+// let age = 25
+// document.write('Current age: ', age)
+// document.write('<br>')
+
+// let maxage = 100
+// document.write(' Estimate maximum age: ', maxage)
+// document.write('<br>')
+
+// let perday = 1000
+// document.write('amount of per day: ', perday)
+// document.write('<br>')
+
+// let restLifeDay = (maxage - age)* perday
+// document.write('You will need ', restLifeDay ,' to last you until the ripe old age of ',maxage)
